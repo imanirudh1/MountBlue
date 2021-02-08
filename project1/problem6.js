7748e2e5310334847ebab96d5eadce5aa5c14f28
Aniruddha Banerjee <imanirudh.dev@gmail.com>
2021-02-08 21:16:31 +0530

    Get all the Audi and BMW cars details

---
+++ project1/problem6.js
@@ -0,0 +1,10 @@
+const problem6 = (inventory) => {
+  const cars = []
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_make === 'Audi' || inventory[i].car_make === 'BMW') {
+      cars.push(inventory[i])
+    }
+  }
+  return JSON.stringify(cars)
+}
+module.exports = problem6
---
+++ project1/testProblem6.js
@@ -0,0 +1,5 @@
+const inventory = require('./inventory.js')
+const problem6 = require('./problem6')
+
+const result = problem6(inventory)
+console.log(result)
