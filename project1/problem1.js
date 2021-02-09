5a82bc008a86fdf435dda815e46efa512f6d5007
Aniruddha Banerjee <imanirudh.dev@gmail.com>
2021-02-09 23:30:04 +0530

    Update problem1.js

--- project1/problem1.js
+++ project1/problem1.js
@@ -1,7 +1,8 @@
 const problem1 = (inventory, carId) => {
+
   for (let i = 0; i < inventory.length; i++) {
     if (inventory[i].id === carId) {
-      return `Car ${carId} is a ${inventory[i].car_year} ${inventory[i].car_make} ${inventory[i].car_model}`
+      return [inventory[i]]
     }
   }
 }
