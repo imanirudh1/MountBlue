c9ddbee1b06fd4b47096f286bfc6ce5080f5613a
Aniruddha Banerjee <imanirudh.dev@gmail.com>
2021-02-10 21:35:51 +0530

    Added jsDoc

--- project1/problem2.js
+++ project1/problem2.js
@@ -1,6 +1,21 @@
+/**
+ * Find the last car details
+ *
+ * @param {Object[]} inventory - The details of the last car in the inventory
+ * @return {string}
+ */
+
 const problem2 = (inventory) => {
+  if (inventory.length === 0 || !Array.isArray(inventory)) return []
   let lastCar = inventory.length - 1
-  return `Last car is a ${inventory[lastCar].car_make} ${inventory[lastCar].car_model}`
+  return [
+    `Last car is a ${inventory[lastCar].car_make} ${inventory[lastCar].car_model}`,
+  ]
 }
+
+/**
+ * A module that finds the last car details!
+ * @module ./problem2
+ */
 
 module.exports = problem2
