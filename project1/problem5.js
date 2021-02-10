438601c781458bc23b34244d4b75c2065afb6eab
Aniruddha Banerjee <imanirudh.dev@gmail.com>
2021-02-10 21:37:21 +0530

    Add jsDocs

--- project1/problem5.js
+++ project1/problem5.js
@@ -1,4 +1,12 @@
+/**
+ * Get all the cars below year 2000
+ *
+ * @param {Array[]} carYears - list of all the years
+ *
+ * @return {Number}
+ */
 const problem5 = (carYears) => {
+  if (carYears.length === 0 || !Array.isArray(carYears)) return []
   carYears.sort((a, b) => a - b)
   let count = 0
   for (let i = 0; i < carYears.length; i++) {
@@ -8,7 +16,12 @@ const problem5 = (carYears) => {
       break
     }
   }
+
   return carYears.slice(0, count)
 }
+/**
+ * A module to get list of car below year 2000 and Length of the list
+ * @module ./problem5
+ */
 
 module.exports = problem5
