950c94c8538deba269cf9ea5733ada9cf9e45735
Aniruddha Banerjee <imanirudh.dev@gmail.com>
2021-02-10 21:36:52 +0530

    Update problem4.js

--- project1/problem4.js
+++ project1/problem4.js
@@ -1,8 +1,22 @@
+/**
+ * Get all the cars year
+ *
+ * @param {Object[]} inventory - The details of the cars
+ *
+ * @return {Array[]}
+ */
+
 const problem4 = (inventory) => {
+  if (inventory.length === 0 || !Array.isArray(inventory)) return []
   var carYears = []
   for (let i = 0; i < inventory.length; i++) {
     carYears.push(inventory[i].car_year)
   }
   return carYears
 }
+/**
+ * A module to get all the cars year
+ * @module ./problem4
+ */
+
 module.exports = problem4
