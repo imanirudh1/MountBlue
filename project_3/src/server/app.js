825fddc2fe68f07fac42ce90a5e7dfd91e5de54f
Aniruddha Banerjee <imanirudh.dev@gmail.com>
2021-02-22 21:22:36 +0530

    Update app.js

--- project_3/src/server/app.js
+++ project_3/src/server/app.js
@@ -1,20 +1,77 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs')
 const app = express()
 
-app.get('/matchesplayed', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/output', 'matchesPlayed.json'))
+const clientPath = path.join(__dirname, 'src', 'client')
+const outputFiles = path.join(__dirname, 'src', 'public', 'output')
+
+app.get('/', (req, res) => {
+  fs.readFile(clientPath + '/index.html', 'utf8', (err, data) => {
+    if (err) {
+      res.status(404)
+      console.error(err)
+    } else {
+      res.set({
+        'Content-Type': 'text/html',
+      })
+      res.status(200)
+      res.send(data)
+    }
+  })
 })
 
-app.get('/extraruns', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/output', 'extraRuns.json'))
+app.get('/:files', (req, res) => {
+  let file = req.params.files
+  console.log(file)
+  const ext = file.split('.')[1]
+  console.log(ext)
+  let contentType
+  switch (ext) {
+    case 'js':
+      contentType = 'text/javascript'
+      break
+    case 'css':
+      contentType = 'text/css'
+      break
+    case 'json':
+      contentType = 'application/json'
+      break
+    case 'png':
+      contentType = 'image/png'
+      break
+    case 'jpg':
+      contentType = 'image/jpg'
+      break
+  }
+  fs.readFile(clientPath + '/' + file, 'utf8', (err, data) => {
+    if (err) {
+      res.status(404)
+      console.error(err)
+    } else {
+      res.set({ 'Content-Type': contentType })
+      res.status(200)
+      res.send(data)
+    }
+  })
 })
 
-app.get('/matcheswon', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/output', 'matchesWon.json'))
-})
+app.get('/api/:name', (req, res) => {
+  let fileName = req.params.name + '.json'
 
-app.use(express.static(path.join(__dirname, '../client')))
+  fs.readFile(outputFiles + '/' + fileName, 'utf8', (err, data) => {
+    if (err) {
+      res.status(404)
+      console.error(err)
+    } else {
+      res.set({
+        'Content-Type': 'application/json',
+      })
+      res.status(200)
+      res.send(data)
+    }
+  })
+})
 
 const port = process.env.PORT || 3000
 
