6315a7016b24e9e5c16cd8fac941613f8726f66a
Aniruddha Banerjee <imanirudh.dev@gmail.com>
2021-02-25 23:17:56 +0530

    Update random.js

--- project_4/random.js
+++ project_4/random.js
@@ -6,6 +6,7 @@ function fetchRandomNumbers() {
       let randomNum = Math.floor(Math.random() * (100 - 0 + 1)) + 0
 
       resolve(randomNum)
+      console.log('Received random number:', randomNum)
     }, (Math.floor(Math.random() * 5) + 1) * 1000)
   })
 }
@@ -27,29 +28,68 @@ function fetchRandomString(callback) {
         )
       }
 
-      console.log('Received random string:')
-
       resolve(result)
+      console.log('Received random string:', result)
     }, (Math.floor(Math.random() * 5) + 1) * 1000)
   })
 }
 
-
 // Task 1
 
-//fetchRandomNumbers().then((res) => console.log(res))
-//fetchRandomString().then((res) => console.log(res))
+fetchRandomNumbers().then((res) => console.log(res))
+fetchRandomString().then((res) => console.log(res))
 
 // Task 2
-// let sum = 0
-// fetchRandomNumbers()
-//   .then((res) => {
-//     sum += res
 
-//     return fetchRandomNumbers()
-//   })
-//   .then((res) => {
-//     sum += res
+let sum = 0
+fetchRandomNumbers()
+  .then((res) => {
+    sum += res
 
-//     console.log(sum)
-//   })
+    return fetchRandomNumbers()
+  })
+  .then((res) => {
+    sum += res
+
+    console.log(sum)
+  })
+  .catch((err) => console.log(err))
+
+// Task 3
+
+let str = ''
+fetchRandomNumbers()
+  .then((res) => {
+    str += res
+
+    return fetchRandomString()
+  })
+  .then((res) => {
+    str += res
+
+    console.log('Concatenated string ', str)
+  })
+  .catch((err) => console.log(err))
+
+// Task 4
+
+Promise.all([
+  fetchRandomNumbers(),
+  fetchRandomNumbers(),
+  fetchRandomNumbers(),
+  fetchRandomNumbers(),
+  fetchRandomNumbers(),
+  fetchRandomNumbers(),
+  fetchRandomNumbers(),
+  fetchRandomNumbers(),
+  fetchRandomNumbers(),
+  fetchRandomNumbers(),
+])
+  .then((res) => {
+    console.log(res)
+
+    let sum = res.reduce((acc, curr) => acc + curr, 0)
+
+    console.log(sum)
+  })
+  .catch((err) => console.log(err))
